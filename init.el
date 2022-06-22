cdb5aab09570f73295319b8fe2fad85c89566698
Adam Ringeisen <ringeisen@gmail.com>
2022-06-21 23:08:47 -0400

    Removed files based on .gitignore

--- init.el
+++ init.el
@@ -16,3 +16,16 @@
 
 ;; load up all literate org-mode files in this directory
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lsp-pyright lsp-mode projectile which-key vertico use-package orderless marginalia dashboard consult ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
