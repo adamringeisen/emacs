25a562fe3469a8cadba5d108349a4bf020799cd4
Adam Ringeisen <ringeisen@gmail.com>
2023-09-21 19:51:25 -0400

    Should be fixed for 29

--- init.el
+++ init.el
@@ -23,23 +23,11 @@
                                             ".." org-dir))))
        (load-path (append (list org-dir org-contrib-dir)
                           (or load-path nil))))
+  )
   ;; load up Org-mode and Org-babel
-  (require 'org-install)
-  (require 'ob-tangle))
+ ;; (require 'org-install)
+  (require 'ob-tangle)
 
 ;; load up all literate org-mode files in this directory
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(js-jsx-indent-level 2)
- ;; '(package-selected-packages
- ;;   '(tab-jump-out free-keys exec-path-from-shell rjsx-mode olivetti olivetti-mode format-all pyvenv org-bullets vterm typescript-mode doom-themes zenburn-theme key-chord expand-region rustic lsp-mode projectile which-key vertico use-package orderless marginalia dashboard consult ace-window)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
